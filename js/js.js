91d23957d40f7b4b068c77c22d0becfaa3e7e45a
Oleg <astral.the.best@gmail.com>
2017-02-23 23:16:59 +0200

    commit

--- js/js.js
+++ js/js.js
@@ -1,5 +1,40 @@
 setTimeout(function(){
-    var name = document.getElementsByTagName('h1');
-    name[0].style.display = 'block';
-    name[0].style.animation = 'greet 2.3s linear';
-},700)
+    var name = document.getElementById('menu');
+    name.style.display = 'block';
+    name.style.animation = 'greet 2.3s linear';
+},700);
+
+$('#greet').flowtype({
+    minimum : 350,
+    maximum : 1920,
+    minFont   : 5,
+    maxFont   : 65
+
+});
+
+var x=0;
+$(".left").click(function() {
+    if(x!=1){
+        $(this).css('transform', 'rotate(180deg)');
+        $('#info').css('left', '0');
+        $('.right').css('display', 'none');
+        $('#menu').css('left', '55%');
+        x+=1;
+    }
+
+        else if(x==1){
+        $(this).css('transform', 'rotate(0deg)');
+        $('#info').css('left', '-580px');
+        $('.right').css('display', 'inline-block');
+        $('#menu').css('left', '50%');
+        x-=1;
+        }
+
+
+});
+
+
+
+
+
+
