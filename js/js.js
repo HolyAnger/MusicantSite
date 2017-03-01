1ef34499249d462d62387100fe84330a01eb25ca
Oleg <astral.the.best@gmail.com>
2017-03-01 21:43:16 +0200

    'commit'

--- js/js.js
+++ js/js.js
@@ -1,9 +1,17 @@
 
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.delay(200).fadeOut('slow');
+    $preloader.delay(650).fadeOut('slow');
+});
+
+
+
     setTimeout(function(){
         var name = document.getElementById('menu');
-        name.style.display = 'block';
         name.style.animation = 'greet 2.3s linear';
-    },700);
+    },800);
 
 
 
@@ -21,15 +29,15 @@
             $(this).css('transform', 'rotate(180deg)');
             $('#info').css('left', '0');
             $('.right').css('display', 'none');
-            $('#menu').css('left', '55%');
+            $('#menu').css({'left':'86%', 'width':'98%', 'text-align':'left'});
             x+=1;
         }
 
         else if(x==1){
             $(this).css('transform', 'rotate(0deg)');
-            $('#info').css('left', '-580px');
+            $('#info').css('left', '-770px');
             $('.right').css('display', 'inline-block');
-            $('#menu').css('left', '50%');
+            $('#menu').css({'left':'50%', 'text-align':'center'});
             x-=1;
         }
     });
@@ -39,7 +47,7 @@
             $(this).css('transform', 'rotate(-180deg)');
             $('#list').css('right', '0');
             $('.left').css('display', 'none');
-            $('#menu').css('left', '35%');
+            $('#menu').css({'left':'14%', 'width':'98%', 'text-align':'right'});
             x+=1;
         }
 
@@ -47,26 +55,26 @@
             $(this).css('transform', 'rotate(0deg)');
             $('#list').css('right', '-770px');
             $('.left').css('display', 'inline-block');
-            $('#menu').css('left', '50%');
+            $('#menu').css({'left':'50%', 'text-align':'center'});
             x-=1;
         }
     });
 
 
-
-
-
-    $('.close').click(function(){
-       $('#modal').fadeOut('slow');
+    $('.close, .overlay').click(function(){
+       $('#modal, .overlay').fadeOut('slow');
         $('#frame').attr('src', '' );
 
     });
 
+
 function value(e){
    var link =  $(e).attr('video');
+    $('.overlay').fadeIn('slow');
     $('#modal').fadeIn('slow');
-    $('#frame').attr('src', link );
+    $('#frame').attr('src', link + "?autoplay=1" );
 }
+
 
 
 
